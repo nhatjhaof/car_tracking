2ab48272493ea97fe4c163fff3e4879cf27bc64a
Your Name <you@example.com>
2025-03-19 20:13:00 +0700

    first commit fix

--- src/main/java/vn/car_service/Car_Service/model/User.java
+++ src/main/java/vn/car_service/Car_Service/model/User.java
@@ -20,10 +20,10 @@ import java.util.*;
 public class User extends AbstractEntity{
 
 
-    @Column(name = "firstName", length = 255)
+    @Column(name = "first_name", length = 255)
     private String firstName;
 
-    @Column(name = "lastName", length = 255)
+    @Column(name = "last_name", length = 255)
     private String lastName;
 
     @Enumerated(EnumType.STRING)
